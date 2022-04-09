74d0500adfac9faddb30c32c4d921c371aa48f22
Ruslan Ustits <ustitsruslan@gmail.com>
2022-04-10 00:59:38 +0300

    Add logo

--- src/main/kotlin/rando/html/Layout.kt
+++ src/main/kotlin/rando/html/Layout.kt
@@ -5,15 +5,20 @@ import kotlinx.html.FlowContent
 import kotlinx.html.HTML
 import kotlinx.html.a
 import kotlinx.html.body
+import kotlinx.html.div
 import kotlinx.html.footer
 import kotlinx.html.head
+import kotlinx.html.li
 import kotlinx.html.link
 import kotlinx.html.main
 import kotlinx.html.meta
+import kotlinx.html.nav
 import kotlinx.html.p
 import kotlinx.html.script
 import kotlinx.html.small
+import kotlinx.html.strong
 import kotlinx.html.title
+import kotlinx.html.ul
 
 class Layout : Template<HTML> {
 
@@ -29,14 +34,28 @@ class Layout : Template<HTML> {
             }
 
             link(rel = "stylesheet", href = "/assets/pico/css/pico.min.css")
+            link(rel = "stylesheet", href = "/assets/rando.css")
             script { src = "/assets/htmx.org/htmx.min.js" }
             script { src = "/assets/hyperscript.org/_hyperscript_web.min.js" }
         }
         body {
+            div("container-fluid") {
+                nav {
+                    ul {
+                        li {
+                            a(href = "./", classes = "contrast logo") {
+                                strong {
+                                    +"Rando"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             main(classes = "container") {
                 insert(content)
             }
-            footer("container") {
+            footer("container-fluid") {
                 small {
                     p { +"by Ruslan Ustits" }
                     a(href = "https://github.com/ustits/rando") { +"Github" }
